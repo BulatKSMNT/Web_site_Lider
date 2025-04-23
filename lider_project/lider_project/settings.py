38b651607968837a5f9629c22490a4b600ada080
Bulat Khatypov <hatypov.ba@gmail.com>
2025-04-23 16:19:38 +0300

    Доделана главная страница

--- lider_project/lider_project/settings.py
+++ lider_project/lider_project/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'lider_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'models',
-        'USER': 'admin',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'models',
+#         'USER': 'admin',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
