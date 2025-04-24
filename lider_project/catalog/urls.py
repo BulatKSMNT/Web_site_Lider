7fcbeb82e0af7e57014cc49eae1de41e8a58d534
Bulat Khatypov <hatypov.ba@gmail.com>
2025-04-24 14:22:30 +0300

    Сделаны шаблоны для каталога, нуңно доделать адаптив: скрытие меню после нажатия, допилить правильное наложение шапки и по мелочи

--- lider_project/catalog/urls.py
+++ lider_project/catalog/urls.py
@@ -1,6 +1,10 @@
+from tkinter.font import names
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.catalog_list, name='catalog_list'),
+    path('category/', views.category, name = 'category'),
+    path('category/product/', views.product, name = 'product')
 ]
--- lider_project/catalog/views.py
+++ lider_project/catalog/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 def catalog_list(request):
     return render(request, 'catalog/catalog_list.html')
+
+def category(request):
+    return render(request, 'catalog/category.html')
+def product(request):
+    return render(request, 'catalog/product.html')
