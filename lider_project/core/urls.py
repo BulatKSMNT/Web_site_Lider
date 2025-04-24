6f036245b77bb58915d8a67c2c0f322580b11299
Natr1xon <sencenkov291@gmail.com>
2025-04-24 22:44:06 +0300

    final request

--- lider_project/core/urls.py
+++ lider_project/core/urls.py
@@ -1,9 +1,7 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.user_request, name='request')
+    path('', views.user_request, name='index')
 ]
